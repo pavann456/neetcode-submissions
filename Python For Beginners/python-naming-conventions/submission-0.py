838e43bf0ba770046b22f6a9f64d40eddfe2170c
pavann456 <sravanpavan05@gmail.com>
2026-07-21 14:52:37 +0530

    Add: python-naming-conventions - submission-0

---
+++ Python For Beginners/python-naming-conventions/submission-0.py
@@ -0,0 +1,7 @@
+python_Creation_date = "February, 1991"
+java_Creation_date = "May, 1995"
+javascript_Creation_date = "December, 1995"
+
+print(python_Creation_date)
+print(java_Creation_date)
+print(javascript_Creation_date)
